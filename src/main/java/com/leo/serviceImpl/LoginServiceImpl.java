336db980265760c6a1ec065f30789ca88a492376
李宝宝 <798112618@qq.com>
2016-12-07 17:52:44 +0800

    提交

---
+++ src/main/java/com/leo/serviceImpl/LoginServiceImpl.java
@@ -0,0 +1,19 @@
+package com.leo.serviceImpl;
+
+import org.aspectj.bridge.ILifecycleAware;
+
+public class LoginServiceImpl implements ILifecycleAware {
+
+	@Override
+	public void buildStarting(boolean isIncremental) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void buildFinished(boolean wasIncremental) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
